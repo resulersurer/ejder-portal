ca8cc3c20d7fd96efe1c83d9fe91df2d870b8231
Resul Ersürer <resul.ersurer@icloud.com>
2026-06-03 17:03:38 +0300

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -19,7 +20,10 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
